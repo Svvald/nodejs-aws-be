c4ef410f15e4ec4578e0e15d6fbc38086f611a94
Nikita Chistousov <nc@svvald.me>
2020-11-02 05:59:06 +0300

    feat: add error handling and unit tests

--- product-service/src/get-products-list.ts
+++ product-service/src/get-products-list.ts
@@ -1,15 +1,25 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { CORS_HEADERS } from './cors-headers';
 import products from './products-list.json';
 
 export const getProductsList: APIGatewayProxyHandler = async (event) => {
   console.log('getProductsList invokation with event: ', event);
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify(products)
+  try {
+    const responseData = JSON.stringify(products);
+    return {
+      statusCode: 200,
+      headers: { ...CORS_HEADERS },
+      body: responseData
+    }
+  } catch (error) {
+    const errorData = JSON.stringify(error);
+    console.log('An error occured while processing event: ', errorData);
+
+    return {
+      statusCode: 500,
+      headers: { ...CORS_HEADERS },
+      body: errorData
+    }
   }
 }
